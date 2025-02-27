8150e6fd935390a54c00e32d5db9d567577dc9a1
Kane Kabena <onekaneldn@gmail.com>
2025-02-27 08:39:47 +0300

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,56 @@
+function add() {
+1 + 80; //=> 81
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+}
+
+function subtract() {
+100 - 50; //=> 50
+40 - 30; //=> 10
+}
+
+function multiply() {
+5 * 10; //=> 50
+10 * 0.5; //=> 5
+}
+
+function divide() {
+100 / 10; //=> 10
+50 / 2; //=> 25
+}
+
+function add(a, b) {
+return a + b;
+}
+
+function subtract(a, b) {
+return a - b;
+}
+
+function multiply(a, b) {
+return a * b;
+}
+
+function divide(a, b) {
+return a / b;
+}
+
+function increment(n) {
+return n += 1;
+}
+
+function decrement(n) {
+return n -= 1;
+}
+
+function makeInt(n) {
+return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+return parseFloat(n);
+}
+
+function preserveDecimal(n) {
+return parseFloat(n);
+}
